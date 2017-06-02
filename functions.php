a1cb24ad9ca682b2ac1f1f534fe760bbec3cc071
Greg Hunt <freshbrewedweb@gmail.com>
2017-06-02 15:09:07 -0400

    Ignore some files

--- functions.php
+++ functions.php
@@ -107,9 +107,9 @@ add_action( 'widgets_init', 'fbw_widgets_init' );
 function fbw_scripts() {
 	wp_enqueue_style( 'fbw-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'fbw-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'fbw-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'fbw-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'fbw-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
